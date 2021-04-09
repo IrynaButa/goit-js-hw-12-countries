c499709265c571c71428ce0ef56c275a93e3065a
Iryna Dagli <dagliirina@ukr.net>
2021-04-09 15:54:04 +0300

    add correct

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,10 +1,12 @@
+import errorNotice from './notice.js'
+
 const BASE_URL = 'https://restcountries.eu/rest/v2';
 
 
 function fetchCountry(countryName) {
   return fetch(`${BASE_URL}/name/${countryName}`).then(response =>
     response.json(),
-  );
+  ).catch(errorNotice);
 }
 
 export default {fetchCountry };
